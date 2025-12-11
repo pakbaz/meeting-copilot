eea226d3716562c553103be64d8b8c3e09d304eb
Sepehr Pakbaz <info@starspak.com>
2025-12-11 00:07:07 -0800

    Commited Fresh Copy

--- src/MeetingCopilot.Contracts/Messages/UtteranceProcessedMessage.cs
+++
@@ -1,8 +0,0 @@
-using MeetingCopilot.Contracts.Entities;
-
-namespace MeetingCopilot.Contracts.Messages;
-
-public record UtteranceProcessedMessage : AgentMessage
-{
-    public Interaction Interaction { get; init; } = default!;
-}
